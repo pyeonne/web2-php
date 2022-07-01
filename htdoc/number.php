9ecb09932c42de4b9beeea71790b567f6f720541
pyeonne <pyeonne@gmail.com>
2022-07-01 13:45:33 +0900

    📖 DOC: PHP 기초

---
+++ htdoc/number.php
@@ -0,0 +1,36 @@
+<!-- php의 모든 코드는 최종적으로 사라진다. -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Number & Arithmetic Operator</h1>
+    <h2>1+1</h2>
+    <?php
+        echo 1+1;
+    ?>
+
+    <h2>2-1</h2>
+    <?php
+        echo 2-1;
+    ?>
+
+    <h2>2*2</h2>
+    <?php
+        echo 2*2;
+    ?>
+
+    <h2>4/2</h2>
+    <?php
+        echo 4/2;
+    ?>
+
+</body>
+
+</html>
---
+++ htdoc/variable.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variable</title>
+</head>
+
+<body>
+    <h1>Variable</h1>
+    <?php
+      $name = 'pyeonne';
+      echo "  Lorem ipsum dolor sit amet consectetur ".$name." adipisicing elit. Vitae magnam consequuntur inventore ad cumque, natus
+        obcaecati delectus harum suscipit, fuga dolorum architecto molestias, modi sunt beatae! Rem at nihil quo."
+    ?>
+
+</body>
+
+</html>
