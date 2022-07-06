0504c283da99ae31ff4d8e4d4c272014a3523d7d
pyeonne <pyeonne@gmail.com>
2022-07-06 09:54:56 +0900

    📖 DOC: 함수

--- htdoc/index.php
+++ htdoc/index.php
@@ -1,3 +1,34 @@
+<?php
+    function print_title()
+    {
+        if (isset($_GET['id'])) {
+            echo $_GET['id'];
+        } else {
+            echo 'WEB';
+        }
+    }
+
+    function print_description()
+    {
+        if (isset($_GET['id'])) {
+            echo file_get_contents("data/".$_GET['id']);
+        } else {
+            echo 'Hello, PHP';
+        }
+    }
+
+    function print_list()
+    {
+        $list = scandir('./data');
+        $i = 0;
+        while ($i<count($list)) {
+            if ($list[$i] != '.' && $list[$i] != '..') {
+                echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>";
+            }
+            $i = $i + 1;
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,39 +36,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WEB</title>
+    <title><?php
+            print_title();
+        ?></title>
 </head>
 
 <body>
     <h1><a href="index.php">WEB</a></h1>
     <ol>
         <?php
-            $list = scandir('./data');
-            $i = 0;
-            while ($i<count($list)) {
-                if ($list[$i] != '.' && $list[$i] != '..') {
-                    echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>";
-                }
-                $i = $i + 1;
-            }
+            print_list();
         ?>
     </ol>
 
     <h2>
         <?php
-            if (isset($_GET['id'])) {
-                echo $_GET['id'];
-            } else {
-                echo 'Welcome';
-            }
+            print_title();
         ?>
     </h2>
     <?php
-        if (isset($_GET['id'])) {
-            echo file_get_contents("data/".$_GET['id']);
-        } else {
-            echo 'Hello, PHP';
-        }
+        print_description();
     ?>
 </body>
 
